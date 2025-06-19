06cadb2cf4992ce04fe367d77c004d2994de8856
Nils Schnakenberg <52465077+ni2scmn@users.noreply.github.com>
2025-06-19 13:26:02 +0200

    plot interculture dist

--- vis_exp_population_diversity.py
+++ vis_exp_population_diversity.py
@@ -2,12 +2,13 @@ import csv
 import ast
 from math import exp
 import numpy as np
-from scipy.spatial.distance import pdist, squareform
+from scipy.spatial.distance import pdist, squareform, cdist
 import os
 import re
 import matplotlib.pyplot as plt
 import concurrent.futures
 from tqdm import tqdm
+import sys
 
 
 def read_weights_from_csv(file_path):
@@ -46,10 +47,11 @@ def diversity_pairwise_distance(population):
         float: The average pairwise distance.
     """
     # print(population.shape)
+    distances_ic = cdist(population[0:5], population[5:10], metric='cosine')
     distances_c1 = pdist(population[0:5], metric='cosine')
     distances_c2 = pdist(population[5:10], metric='cosine')
     distances = pdist(population, metric='cosine')
-    return (np.mean(distances_c1),  np.mean(distances_c2), np.mean(distances))
+    return (np.mean(distances_ic), np.mean(distances_c1), np.mean(distances_c2), np.mean(distances))
 
 def plot_diversity_over_timesteps(data):
     """
@@ -58,13 +60,15 @@ def plot_diversity_over_timesteps(data):
     Args:
         data (list): A list of tuples containing timestep and population.
     """
+    avg_distances_ic = []
     avg_distances_c1 = []
     avg_distances_c2 = []
     avg_distances = []
     timesteps = []
 
     for timestep, population in data:
-        avg_distance_c1, avg_distance_c2, avg_distance = diversity_pairwise_distance(np.array(population))
+        avg_distance_ic, avg_distance_c1, avg_distance_c2, avg_distance = diversity_pairwise_distance(np.array(population))
+        avg_distances_ic.append(avg_distance_ic)
         avg_distances_c1.append(avg_distance_c1)
         avg_distances_c2.append(avg_distance_c2)
         avg_distances.append(avg_distance)
@@ -90,37 +94,46 @@ def plot_diversity_over_timesteps_multi(runs_data):
     """
     # Find the minimum length of runs to align timesteps
     min_len = min(len(run) for run in runs_data)
+    all_distances_ic = []
     all_distances_c1 = []
     all_distances_c2 = []
     all_distances = []
 
     # Compute diversity for each run and timestep
     for ridx, run in enumerate(runs_data):
+        run_distances_ic = []
         run_distances_c1 = []
         run_distances_c2 = []
         run_distances = []
         for timestep, population in run[:min_len]:
-            avg_distance_c1, avg_distance_2, avg_distance = diversity_pairwise_distance(np.array(population))
+            avg_distance_ic, avg_distance_c1, avg_distance_2, avg_distance = diversity_pairwise_distance(np.array(population))
+            run_distances_ic.append(avg_distance_ic)
             run_distances_c1.append(avg_distance_c1)
             run_distances_c2.append(avg_distance_2)
             run_distances.append(avg_distance)
+        all_distances_ic.append(run_distances_ic)
         all_distances_c1.append(run_distances_c1)
         all_distances_c2.append(run_distances_c2)
         all_distances.append(run_distances)
         # print(f"Run {ridx}, Avg Distance = {run_distances}")
 
+    all_distances_ic = np.array(all_distances_ic)  # shape: (n_runs, n_timesteps)
     all_distances_c1 = np.array(all_distances_c1)  # shape: (n_runs, n_timesteps)
     all_distances_c2 = np.array(all_distances_c2)  # shape:
     all_distances = np.array(all_distances)  # shape: (n_runs, n_timesteps)
+    avg_distances_ic = np.mean(all_distances_ic, axis=0)
     avg_distances_c1 = np.mean(all_distances_c1, axis=0)
     std_distances_c1 = np.std(all_distances_c1, axis=0)
     avg_distances = np.mean(all_distances, axis=0)
+    std_distances_ic = np.std(all_distances_ic, axis=0)
     std_distances_c1 = np.std(all_distances_c1, axis=0)
     avg_distances_c2 = np.mean(all_distances_c2, axis=0)
     std_distances = np.std(all_distances, axis=0)
     timesteps = list(range(min_len))
+    timesteps = [t * 10 for t in timesteps]  # Adjust to capture the 10x timesteps
 
     plt.figure(figsize=(10, 5))
+    plt.plot(timesteps, avg_distances_ic, marker='o', label='IC Average Diversity')
     plt.plot(timesteps, avg_distances_c1, marker='o', label='C1 Average Diversity')
     plt.plot(timesteps, avg_distances_c2, marker='o', label='C2 Average Diversity')
     plt.plot(timesteps, avg_distances, marker='o', label='Average Diversity')
@@ -138,7 +151,7 @@ def plot_diversity_over_timesteps_multi(runs_data):
 
 if __name__ == "__main__":
     # Find all nn_weights_*.csv files in the directory
-    run_dir = "data/e_ca_nn_0/1750255046"
+    run_dir = sys.argv[1]
     run_files = sorted([f for f in os.listdir(run_dir) if re.match(r"nn_weights_\d+\.csv", f)])
     run_files = run_files[:5]  # Limit to first 5 files for testing
     runs_data = []
